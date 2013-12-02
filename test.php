1b24924bc8a16732176dff559f5f0a958ca9f060
James Waples <jwaples@longtallsally.co.uk>
2013-12-02 11:12:04 +0000

    Added test page

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'Translator.php';
+?>
+
+<h1>Tee testing</h1>
+
+<h2>Simple test</h2>
+
+<pre>
+<code>&lt;?= T("Hello world, my name is %s", 'James') ?></code>
+</pre>
+
+<p>
+	<?= T("Hello world, my name is %s", 'James') ?>
+</p>
+
+<hr>
