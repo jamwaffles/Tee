fd695e6492585e5ffeab6528fe6acd6aaf6ae42a
James Waples <jwaples@longtallsally.co.uk>
2013-12-02 11:18:05 +0000

    Added PHPDoc doc comments

--- Tee/Translator.php
+++ Tee/Translator.php
@@ -1,6 +1,24 @@
 <?php
+/**
+* @author James Waples
+* @since v0.0.1
+* 
+* Main translator class
+*/
 namespace Tee {
 	class Translator {
+		/**
+		* @since v0.0.1
+		* @static
+		* Take a string template and either a list or array of data values to insert
+		*
+		* @param String $string The template string compatible with <code>sprintf()</code>
+		*
+		* @param Array $args Array of values to insert into the template string
+		* @param Multiple $args[...] A list of separate arguments to insert into the template string
+		*
+		* @return String The compiled string
+		*/
 		public static function translate() {
 			$args = func_get_args();
 			$string = array_shift($args);
@@ -18,6 +36,9 @@ namespace Tee {
 }
 
 namespace {
+	/**
+	* @global Function T
+	*/
 	function T($string, $args = null) {
 		return \Tee\Translator::translate($string, $args);
 	}
