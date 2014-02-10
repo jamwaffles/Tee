3c7fe41514be1e3d2dc2f6fc428efd515e72feb9
James Waples <jwaples@longtallsally.co.uk>
2014-02-10 16:07:23 +0000

    Added some more test cases

--- test.php
+++ test.php
@@ -17,10 +17,13 @@ Trans::configure(array(
 <code>&lt;?= T("Hello world, my name is Kerbal") ?>
 &lt;?= T("Hello world, my name is %s", 'James') ?>
 &lt;?= T("Hello world, my name is %s", array('Bob')) ?></code>
+&lt;?= T("Hello %s, my name is %s", array('Earth', 'Bob')) ?></code>
+&lt;?= T("Hello %s, my name is %s", 'Earth', 'Bob') ?></code>
 </pre>
 
 <?= T("Hello world, my name is Kerbal") ?><br>
 <?= T("Hello world, my name is %s", 'James') ?><br>
-<?= T("Hello world, my name is %s", array('Bob')) ?>
-
+<?= T("Hello world, my name is %s", array('Bob')) ?><br>
+<?= T("Hello %s, my name is %s", array('Earth', 'Bob')) ?><br>
+<?= T("Hello %s, my name is %s", 'Earth', 'Bob') ?>
 <hr>
